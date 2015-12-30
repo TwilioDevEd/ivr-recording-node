7358e13681174da13494ef297df70c793d019e50
Agustin Camino <agustin.camino@gmail.com>
2015-12-30 11:57:33 -0500

    Add ivr/welcome route

---
+++ routes/ivr.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var twilio = require('twilio');
+
+// POST: /ivr/welcome
+router.post('/welcome', twilio.webhook({validate: false}), function (req, res) {
+    var twiml = new twilio.TwimlResponse();
+    twiml.gather({
+        action: "",
+        numDigits: "1",
+        method: "POST"
+    }, function (node) {
+        node.play("http://howtodocs.s3.amazonaws.com/et-phone.mp3", {loop: 3});
+    });
+
+    res.send(twiml);
+});
+
+module.exports = router;
---
+++ spec/ivr.spec.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+var supertest = require('supertest');
+var cheerio = require('cheerio');
+
+var app = require('../app.js');
+
+describe('ivr', function () {
+  describe('GET /ivr/welcome', function () {
+    it('response contains Gather', function (done) {
+      var agent = supertest(app);
+      agent
+        .post('/ivr/welcome')
+        .expect(function (res) {
+          var $ = cheerio.load(res.text);
+          expect($('Gather').children('Play').length).to.equal(1);
+        })
+        .expect(200, done);
+    });
+  });
+});
