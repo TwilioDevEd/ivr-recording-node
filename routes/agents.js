2cf0d40c2eab9fe20773e949b7a3103dd9907adc
Agustin Camino <agustin.camino@gmail.com>
2015-12-31 08:58:13 -0500

    Add agents/call endpoint

--- routes/agents.js
+++ routes/agents.js
@@ -11,4 +11,38 @@ router.get('/', function (req, res) {
     });
 });
 
+// POST: /agents/call
+router.post('/call', twilio.webhook({validate: false}), function (req, res) {
+  if (req.body.CallStatus === 'completed') {
+    return res.send('');
+  }
+
+  var twiml = new twilio.TwimlResponse();
+  twiml
+    .say("It appears that no agent is available. " +
+         "Please leave a message after the beep",
+         { voice: "alice", language: "en-GB" })
+    .record({
+      maxLength: 20,
+      action: '/agents/hangup',
+      transcribeCallback: '/recordings/create?agentId=' + req.body.agentId
+    })
+    .say("No record received. Goodbye",
+        { voice: "alice", language: "en-GB" })
+    .hangup();
+
+  res.send(twiml);
+});
+
+// POST: /agents/hangup
+router.post('/hangup', twilio.webhook({validate: false}), function (req, res) {
+  var twiml = new twilio.TwimlResponse();
+  twiml
+    .say("Thanks for your message. Goodbye",
+         { voice: "alice", language: "en-GB" })
+    .hangup();
+
+  res.send(twiml);
+});
+
 module.exports = router;
--- spec/agents.spec.js
+++ spec/agents.spec.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var supertest = require('supertest');
+var cheerio = require('cheerio');
 var Agent = require('../models/agent');
 
 var app = require('../app.js');
@@ -25,6 +26,7 @@ describe('agents', function () {
     after(function (done) {
       Agent.remove({}, done);
     });
+
     it('renders a list of agents and recordings', function (done) {
       var agent = supertest(app);
       agent
@@ -34,6 +36,57 @@ describe('agents', function () {
         expect(res.text).to.contain('transcription');
         expect(res.text).to.contain('555 5556');
       })
+      .expect(200, done);
+    });
+  });
+
+  describe('POST /agents/call', function () {
+    context('when the call status is completed', function () {
+      it('responds with empty content', function (done) {
+        var agent = supertest(app);
+        agent
+          .post('/agents/call')
+          .send({ CallStatus: 'completed' })
+          .expect(function (res) {
+            expect(res.text).to.not.contain('Response');
+          })
+        .expect(200, done);
+      });
+    });
+
+    context('when the call status is not completed', function () {
+      it('responds with empty content', function (done) {
+        var agent = supertest(app);
+        agent
+          .post('/agents/call')
+          .send({
+            CallStatus: 'in-progress',
+            agentId: 5
+          })
+          .expect(function (res) {
+            var $ = cheerio.load(res.text);
+            expect($('Say').length).to.equal(2);
+            expect($('Record').first().attr('transcribecallback'))
+              .to.equal('/recordings/create?agentId=5');
+            expect($('Record').first().attr('action'))
+              .to.equal('/agents/hangup');
+            expect($('Hangup').length).to.equal(1);
+          })
+        .expect(200, done);
+      });
+    });
+  });
+
+  describe('POST /agents/hangup', function () {
+    it('responds with say and hangup', function (done) {
+      var agent = supertest(app);
+      agent
+        .post('/agents/hangup')
+        .expect(function (res) {
+          var $ = cheerio.load(res.text);
+          expect($('Say').length).to.equal(1);
+          expect($('Hangup').length).to.equal(1);
+        })
       .expect(200, done);
     });
   });
