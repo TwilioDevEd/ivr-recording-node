cdeb78f3e9e280e740cf52e7c6bb1302fbffb831
Agustin Camino <agustin.camino@gmail.com>
2015-12-31 10:31:55 -0500

    Update actions for extension/connect

--- routes/extension.js
+++ routes/extension.js
@@ -1,28 +1,46 @@
-var express = require('express');
-var router = express.Router();
-var twilio = require('twilio');
-var Agent = require('../models/agent');
+var express = require('express')
+  , router = express.Router()
+  , twilio = require('twilio')
+  , Agent = require('../models/agent');
 
 // POST: /extension/connect
 router.post('/connect', twilio.webhook({validate: false}), function (req, res) {
-  var extension = req.body.Digits;
+  var selectedOption = req.body.Digits,
+      extensions = {
+        2: 'brodo',
+        3: 'dagobah',
+        4: 'oober'
+      };
 
-  Agent.findOne({ extension: extension })
-  .then(function (agent) {
-    var twiml = new twilio.TwimlResponse();
-    twiml
-    .say("You'll be connected shortly to your planet.", { voice: "alice", language: "en-GB" })
-    .dial({ action: '' }, function() {
-      this.number(agent.phoneNumber, {
-        url: ''
-      });
-    });
+  Agent.findOne({ extension: extensions[selectedOption] })
+    .then(function (agent) {
+      if (agent === null) {
+        return res.send(redirectToWelcome());
+      }
 
-    res.send(twiml);
-  })
+      var twiml = new twilio.TwimlResponse();
+      twiml
+        .say("You'll be connected shortly to your planet.",
+             { voice: "alice", language: "en-GB" })
+        .dial({ action: '/agents/call?agentId=' + agent.id }, function() {
+          this.number(agent.phoneNumber, {
+            url: '/agents/screencall'
+          });
+        });
+
+      res.send(twiml);
+    })
   .catch(function (err) {
     console.log(err);
   });
 });
+
+var redirectToWelcome = function () {
+  var twiml = new twilio.TwimlResponse();
+  twiml
+    .redirect('/ivr/welcome');
+
+  return twiml;
+}
 
 module.exports = router;
--- spec/extension.spec.js
+++ spec/extension.spec.js
@@ -1,33 +1,59 @@
 require('./connectionHelper');
 
-var expect = require('chai').expect;
-var supertest = require('supertest');
-var cheerio = require('cheerio');
-var Agent = require('../models/agent');
-
-var app = require('../app.js');
+var expect = require('chai').expect
+  , supertest = require('supertest')
+  , cheerio = require('cheerio')
+  , app = require('../app.js')
+  , Agent = require('../models/agent');
 
 describe('extension', function () {
   describe('POST /extension/connect', function () {
+    var brodo; // Agent reference
+
     before(function (done) {
-      Agent.create({ extension: '123', phoneNumber: '555 5555' }, done);
+      Agent.create({ extension: 'brodo', phoneNumber: '555 5555' })
+        .then(function (agent) {
+          brodo = agent;
+          done();
+        });
     });
 
     after(function (done) {
       Agent.remove({}, done);
     });
 
-    it('responds with return instructions', function (done) {
-      var agent = supertest(app);
-      agent
-      .post('/extension/connect')
-      .send({ Digits: 123 })
-      .expect(function (res) {
-        var $ = cheerio.load(res.text);
-        expect($('Say').length).to.equal(1);
-        expect($('Dial').first().text()).to.equal("555 5555");
-      })
-      .expect(200, done);
+    context('when there is an agent for a given extension', function () {
+
+      it('responds with return instructions', function (done) {
+        var agent = supertest(app);
+        agent
+          .post('/extension/connect')
+          .send({ Digits: 2 })
+          .expect(function (res) {
+            var $ = cheerio.load(res.text);
+            expect($('Say').length).to.equal(1);
+            expect($('Dial').first().attr('action'))
+              .to.equal("/agents/call?agentId=" + brodo.id);
+            expect($('Dial').first().text()).to.equal("555 5555");
+            expect($('Dial').children().first().attr('url'))
+              .to.equal('/agents/screencall');
+          })
+        .expect(200, done);
+      });
+    });
+
+    context('when there is not an agent for a given extension', function () {
+      it('response contains redirect to menu', function (done) {
+        var agent = supertest(app);
+        agent
+          .post('/extension/connect')
+          .send({ Digits: 0 })
+          .expect(function (res) {
+            var $ = cheerio.load(res.text);
+            expect($('Redirect').first().text()).to.equal('/ivr/welcome');
+          })
+        .expect(200, done);
+      });
     });
   });
 });
