16d6a0fd573a09d227bc65eed72a3d3f135f7f8c
Hector Ortega <hector.oj@gmail.com>
2016-11-18 11:45:57 -0500

    Update seeded extension names
    to make them match with those in code and
    hence make them selectable

--- seed/agents.js
+++ seed/agents.js
@@ -2,8 +2,8 @@ db.dropDatabase();
 
 var agents = [
   { extension: 'Brodo',   phoneNumber: '+15552483591' },
-  { extension: 'Dugobah', phoneNumber: '+15558675309' },
-  { extension: '113',     phoneNumber: '+15553185602' }
+  { extension: 'Dagobah', phoneNumber: '+15558675309' },
+  { extension: 'Oober',     phoneNumber: '+15553185602' }
 ];
 
 db.agents.insert(agents);
